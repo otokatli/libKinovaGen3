dade6138cf5f40b775db6a67f5f08a78a2a846e8
Ozan Tokatli <ozan.tokatli@gmail.com>
2023-06-23 13:54:24 +0100

    added docstring

--- include/KinovaGen3.h
+++ include/KinovaGen3.h
@@ -7,17 +7,55 @@ public:
     KinovaGen3();
     ~KinovaGen3();
 
+    /**
+     * Calculate the forward kinematics of the Kinova Gen3 robot
+     * 
+     * @param q Joint positions of the robot
+     * @return The homogenous coordinate of the end-effector [R, x; 0, 1]
+    */
     Eigen::Transform<double, 3, Eigen::Affine> forwardKinematics(const Eigen::Vector<double, 7> q);
 
+    /**
+     * Calculate the inverse kinematics of the Kinova Gen3 robot at velocity level
+     * 
+     * @param q Joint positions of the robot
+     * @param xp End-effector twist of the robot
+     * @return Joint velocities of the robot
+    */
     Eigen::Vector<double, 7> inverseKinematics(const Eigen::Vector<double, 7> q,
                                                const Eigen::Vector<double, 6> xp);
 
+    /**
+     * Calculate the Jacobian of the Kinova Gen3 robot
+     * 
+     * @param q Joint positions of the robot
+     * @return The Jacobian of the robot evaluated at q
+    */
     Eigen::Matrix<double, 6, 7> jacobian(const Eigen::Vector<double, 7> q);
 
+    /**
+     * Calculate the mass matrix of the Kinova Gen3 robot
+     * 
+     * @param q Joint positions of the robot
+     * @return The mass matrix of the robot evaluated at q
+    */
     Eigen::Matrix<double, 7, 7> massMatrix(const Eigen::Vector<double, 7> q);
 
+    /**
+     * Calculate the Coriolis matrix of the Kinova Gen3 robot
+     * 
+     * @param q Joint positions of the robot
+     * @param qp Joint velocities of the robot
+     * @return The Coriolis matrix of the robot evaluated at q and qp
+    */
     Eigen::Vector<double, 7> coriolis(const Eigen::Vector<double, 7> q,
                                       const Eigen::Vector<double, 7> qp);
 
+    /**
+     * Calculate the gravity term of the Kinova Gen3 robot
+     * 
+     * @param q Joint positions of the robot
+     * @return The gravity term of the robot evaluated at q
+    */
     Eigen::Vector<double, 7> gravity(const Eigen::Vector<double, 7> q);
 };
