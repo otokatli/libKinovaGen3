0ce1a996a12b845791e7294454cf62ba1677665a
Ozan Tokatli <ozan.tokatli@ukaea.uk>
2023-07-13 09:20:59 +0100

    removed templates

--- include/KinovaGen3.h
+++ include/KinovaGen3.h
@@ -4,7 +4,6 @@
 #include <Eigen/Dense>
 
 
-template<class T>
 class KinovaGen3
 {
 public:
@@ -17,7 +16,7 @@ public:
      * @param q Joint positions of the robot
      * @return The homogenous coordinate of the end-effector [R, x; 0, 1]
     */
-    Eigen::Transform<T, 3, Eigen::Affine> forwardKinematics(const Eigen::Vector<T, 7>& q);
+    Eigen::Transform<double, 3, Eigen::Affine> forwardKinematics(const Eigen::Vector<double, 7>& q);
 
     /**
      * Calculate the inverse kinematics of the Kinova Gen3 robot at velocity level
@@ -26,8 +25,8 @@ public:
      * @param xp End-effector twist of the robot
      * @return Joint velocities of the robot
     */
-    Eigen::Vector<T, 7> inverseKinematics(const Eigen::Vector<T, 7>& q,
-                                          const Eigen::Vector<T, 6>& xp);
+    Eigen::Vector<double, 7> inverseKinematics(const Eigen::Vector<double, 7>& q,
+                                               const Eigen::Vector<double, 6>& xp);
 
     /**
      * Calculate the Jacobian of the Kinova Gen3 robot
@@ -35,7 +34,7 @@ public:
      * @param q Joint positions of the robot
      * @return The Jacobian of the robot evaluated at q
     */
-    Eigen::Matrix<T, 6, 7> jacobian(const Eigen::Vector<T, 7>& q);
+    Eigen::Matrix<double, 6, 7> jacobian(const Eigen::Vector<double, 7>& q);
 
     /**
      * Calculate the mass matrix of the Kinova Gen3 robot
@@ -43,7 +42,7 @@ public:
      * @param q Joint positions of the robot
      * @return The mass matrix of the robot evaluated at q
     */
-    Eigen::Matrix<T, 7, 7> massMatrix(const Eigen::Vector<T, 7>& q);
+    Eigen::Matrix<double, 7, 7> massMatrix(const Eigen::Vector<double, 7>& q);
 
     /**
      * Calculate the Coriolis matrix of the Kinova Gen3 robot
@@ -52,8 +51,8 @@ public:
      * @param qp Joint velocities of the robot
      * @return The Coriolis matrix of the robot evaluated at q and qp
     */
-    Eigen::Vector<T, 7> coriolis(const Eigen::Vector<T, 7>& q,
-                                      const Eigen::Vector<T, 7>& qp);
+    Eigen::Vector<double, 7> coriolis(const Eigen::Vector<double, 7>& q,
+                                      const Eigen::Vector<double, 7>& qp);
 
     /**
      * Calculate the gravity term of the Kinova Gen3 robot
@@ -61,7 +60,7 @@ public:
      * @param q Joint positions of the robot
      * @return The gravity term of the robot evaluated at q
     */
-    Eigen::Vector<T, 7> gravity(const Eigen::Vector<T, 7>& q);
+    Eigen::Vector<double, 7> gravity(const Eigen::Vector<double, 7>& q);
 };
 
 #endif // KINOVA_GEN_3_H_
